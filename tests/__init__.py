4a4210875b95ba9004d51e7d016fcc995a8232f4
derwentx <derwentx@gmail.com>
2020-06-14 15:37:11 +1000

    exit after post mortem

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,7 @@ import os
 import unittest
 import traceback
 import pdb
+import pytest
 
 TESTS_DIR = os.path.dirname(__file__)
 REPO_DIR = os.path.dirname(TESTS_DIR)
@@ -20,11 +21,13 @@ def debugTestRunner(post_mortem=None, *args, **kwargs):
             traceback.print_exception(*err)
             post_mortem(err[2])
             super(DebugTestResult, self).addError(test, err)
+            pytest.exit('exit after post mortem')
 
         def addFailure(self, test, err):
             traceback.print_exception(*err)
             post_mortem(err[2])
             super(DebugTestResult, self).addFailure(test, err)
+            pytest.exit('exit after post mortem')
 
     kwargs['resultclass'] = DebugTestResult
     return unittest.TextTestRunner(*args, **kwargs)
